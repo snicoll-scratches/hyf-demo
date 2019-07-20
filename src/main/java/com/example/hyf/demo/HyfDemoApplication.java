9540cb014e19c1ce9e0f2d5bc561ed1201cf244b
Stephane Nicoll <snicoll@pivotal.io>
2019-07-20 18:21:40 +0200

    Add demo project

---
+++ src/main/java/com/example/hyf/demo/HyfDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.hyf.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyfDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyfDemoApplication.class, args);
+	}
+
+}
